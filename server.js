d4e0a0512fc65b254a7f244bfcf1621f3ed3ee8f
Amir31-scurt <111583093+Amir31-scurt@users.noreply.github.com>
2025-04-01 23:22:38 +0000

    fix supabase

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const upload = multer({
   storage: multer.memoryStorage(), // Store files in memory for further processing
 });
 
-export const pool = new Pool({
+export const pool = new pg.Pool({
   host: '/var/run/postgresql', // cPanel socket path
   user: 'c2554004c_amir31',    // MUST match your working psql username
   password: 'Admin@admin.com',   // Your verified password
