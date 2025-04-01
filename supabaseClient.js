b349af49a656d13da0abe1c0f712c6b58f354a9c
Amir31-scurt <111583093+Amir31-scurt@users.noreply.github.com>
2025-04-01 23:11:09 +0000

    fix supabase

--- supabaseClient.js
+++ supabaseClient.js
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 // Load environment variables
 dotenv.config();
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = "https://omhzhgowylojwknlmrqp.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9taHpoZ293eWxvandrbmxtcnFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU0Nzk1OTYsImV4cCI6MjA1MTA1NTU5Nn0.09iaLH4V7PWLjk0hGVx7PjjMZ_gjRNxYuIAfSevjTms";
 
 if (!supabaseUrl || !supabaseKey) {
   console.error("Supabase URL or Key is missing!");
