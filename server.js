b9a2f76d6ce13070f3e4e721a3ebd473482693e3
Amir31-scurt <111583093+Amir31-scurt@users.noreply.github.com>
2026-03-04 01:08:28 +0000

    fix: port

--- server.js
+++ server.js
@@ -273,7 +273,7 @@ await server.start();
 server.applyMiddleware({ app });
 
 // Start the server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
   console.log(
