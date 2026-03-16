42261c4ea94d0b0e33878a1eee41ad863434f0ec
Amir31-scurt <111583093+Amir31-scurt@users.noreply.github.com>
2026-03-16 15:57:17 +0000

    fix: server port

--- server.js
+++ server.js
@@ -341,7 +341,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.SERVER_PORT || 4000;
+const PORT = process.env.SERVER_PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   
